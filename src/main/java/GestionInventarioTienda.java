3d5f45476b4bb1dfeb6d541bdfa8ff2d7a317f1b
Francisco <f.lizama05@ufromail.cl>
2024-09-04 22:32:36 -0400

    Creada funcion para verificar existencia de un producto en el inventario, funcion agregarProductos resumida, Agregada funcion restarProductos, agregadas funciones de sumar y restar 2 numeros

--- src/main/java/GestionInventarioTienda.java
+++ src/main/java/GestionInventarioTienda.java
@@ -14,14 +14,22 @@ public class GestionInventarioTienda {
 
     public static void agregarProductos(Object[][] productos, int idProducto, int cantidad){
         int fila = buscarFilaProducto(productos, idProducto);
-        if (fila == -1){
-            System.out.println("Error: No existe el producto");
-        } else {
-            int cantidadActual = (int) productos[fila][2];
-            productos[fila][2] = cantidad + cantidadActual;
-            System.out.println("Aumento de stock exitoso");
-        }
+        int cantidadActual = (int) productos[fila][2];
+        productos[fila][2] = cantidad + cantidadActual;
+        System.out.println("Aumento de stock exitoso");
+    }
 
+    public static void restarProductos(Object[][] productos, int idProducto, int cantidad){
+        int fila = buscarFilaProducto(productos, idProducto);
+        int cantidadActual = (int) productos[fila][2];
+        int resta = restarNumeros(cantidadActual, cantidad);
+        if (resta >= 0){
+            productos[fila][2] = resta;
+            System.out.print("Resta de stock exitoso, nueva cantidad: ");
+            System.out.println(resta);
+        } else {
+            System.out.println("Error: No pueden haber productos en negativo");
+        }
     }
 
     public static int buscarFilaProducto(Object[][] productos, int idProducto){
@@ -38,5 +46,17 @@ public class GestionInventarioTienda {
 
         }
         return filaDelProducto;
+    }
+
+    public static boolean existeProducto(Object[][] productos, int idProducto){
+        return buscarFilaProducto(productos, idProducto) != -1;
+    }
+
+    public static int sumarNumeros(int numero1, int numero2){
+        return numero1 + numero2;
+    }
+
+    public static int restarNumeros(int numero1, int numero2){
+        return numero1 - numero2;
     }
 }
